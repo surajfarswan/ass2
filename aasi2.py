e0b2ebb4222aee5bf51d221c111a35b7cb98276b
surajfarswan <42631838+surajfarswan@users.noreply.github.com>
2018-08-29 23:07:18 +0530

    Add files via upload
    ass2

---
+++ aasi2.py
@@ -0,0 +1,37 @@
+#ques1
+print('i am suraj')
+#ques2
+print("acad"+"view")
+#ques3
+x=int(input("enter value of x\n"))
+y=int(input("enter value of y\n"))
+z=int(input("enter value of z\n"))
+print("value of x",x)
+print("value of y",y)
+print("value of z",z)
+#ques4
+print("let's get started")
+#ques5
+s="Acadview"
+print("s=",s)
+course="python"
+print("course=",course)
+fees=5000
+print("fees=",fees)
+#ques6
+r=int(input("enter the radius\n"))
+area=3.14*r*r
+print(area)
+
+
+
+
+
+
+
+
+
+
+
+
+
